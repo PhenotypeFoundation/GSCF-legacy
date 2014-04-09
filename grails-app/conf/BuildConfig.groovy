2050a4dbb989873e731a3c3fb3fa705a9dceb606
Ferry Jagers <FerryJagers@gmail.com>
2014-04-09 12:17:42 +0200

    dbxpBase (-> GDT) version up

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -76,7 +76,7 @@ grails.project.dependency.resolution = {
 		)
 
 		compile(
-            ":dbxp-base:0.1.3.2.rc1",
+            ":dbxp-base:0.1.3.2.rc2",
             ":dbxp-sam:0.9.6.3.rc8",
 
             ":hibernate:$grailsVersion",
